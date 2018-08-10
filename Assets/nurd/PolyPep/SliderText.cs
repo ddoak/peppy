bb7effbd4f17d17166491c99486d776d2686043c
DGD-PC\ddoak <misc@ddoak.com>
2018-08-10 18:18:52 +0100

    slider values displayed

---
+++ Assets/nurd/PolyPep/SliderText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+
+	Text textComponent;
+
+	void Start()
+	{
+		textComponent = GetComponent<Text>();
+	}
+
+	public void SetSliderValue(float sliderValue)
+	{
+		textComponent.text = Mathf.Round(sliderValue/1).ToString();
+	}
+
+	public void Dummy()
+	{
+
+	}
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
