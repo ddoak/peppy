812096e24c44227775e0bc4329555bf84bb7508b
ddoak <spam@ddoak.com>
2018-11-19 16:34:17 +0000

    pathfinding for adding sidechains
    creating chiral sp3 carbons

---
+++ Assets/nurd/PolyPep/Csp3.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Csp3 : MonoBehaviour {
+
+	public Vector3 posA = new Vector3(0f, 0f, 0f);
+	public Vector3 posB = new Vector3(0f, 0f, 0f);
+	public Vector3 posC = new Vector3(0f, 0f, 0f);
+
+	public List<Transform> HtfList = new List<Transform>();
+	public List<Transform> BtfList = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+
+		SetupSp3Atom();
+
+	}
+	
+	void SetupSp3Atom()
+	{
+
+		Vector3 socketPos = new Vector3(0f, 0f, 0f);
+
+		foreach (Transform child in GetComponentsInChildren<Transform>())
+		{
+			//Debug.Log(child);
+			float _scale = 0.1f; // 0.075f;
+			bool addSocketOffset = true;
+			switch (child.name)
+			{
+				case "H_0":
+					//Debug.Log("----> Found a H_0");
+					HtfList.Add(child);
+					socketPos = new Vector3(1, 0, -1 / Mathf.Sqrt(2));
+					break;
+				case "H_1":
+					HtfList.Add(child);
+					socketPos = new Vector3(-1, 0, -1 / Mathf.Sqrt(2));
+					break;
+				case "H_2":
+					HtfList.Add(child);
+					socketPos = new Vector3(0, 1, 1 / Mathf.Sqrt(2));
+					break;
+				case "H_3":
+					HtfList.Add(child);
+					socketPos = new Vector3(0, -1, 1 / Mathf.Sqrt(2));
+					break;
+
+				case "tf_bond_H0":
+				case "tf_bond_H1":
+				case "tf_bond_H2":
+				case "tf_bond_H3":
+					BtfList.Add(child);
+					addSocketOffset = false;
+					break;
+
+				default:
+					addSocketOffset = false;
+					break;
+			}
+			if (addSocketOffset)
+			{
+				child.transform.position += socketPos * _scale;
+			}
+
+		}
+
+		int j = 0;
+		foreach (Transform _H in HtfList)
+		{
+			//Debug.Log("H List ---> " + _H.name + " " + BtfList[j]);
+
+			posA = transform.position;
+			posB = _H.transform.position; //  HtfList[j].position;
+			Vector3 relativePos = posB - posA;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			BtfList[j].transform.LookAt(posB);// (HtfList[j].position);
+
+			j++;
+		}
+
+		// random rotation to try to avoid axis aligned bugs
+		transform.rotation = Random.rotation;
+
+		CollidersOff();
+	}
+
+	void CollidersOff()
+	{
+			foreach (var col in GetComponentsInChildren<Collider>())
+			{
+				col.isTrigger = true;
+			}
+	}
+
+
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
---
+++ Assets/nurd/PolyPep/SideChainBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideChainBuilder : MonoBehaviour {
+
+
+	public GameObject Csp3_pf;
+	public List<GameObject> SideChainList = new List<GameObject>();
+
+	int sideChainLength = 4;
+
+	public Vector3 posA = new Vector3(0f, 0f, 0f);
+	public Vector3 posB = new Vector3(0f, 0f, 0f);
+	public Vector3 posC = new Vector3(0f, 0f, 0f);
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < sideChainLength; i++)
+		{
+			SideChainList.Add(Instantiate(Csp3_pf, transform.position + (transform.right * i * 0.6f), Quaternion.identity));
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(posA, 0.04f);
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(posB, 0.04f);
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(posC, 0.2f);
+		//if (lastHit)
+		//{
+		//	Gizmos.color = Color.black;
+		//	Gizmos.DrawWireSphere(myHitPos, 0.04f);
+		//}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
