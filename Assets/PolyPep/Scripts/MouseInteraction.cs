4210fc3814b1569c717abc0d13c4799a45292744
ddoak <spam@ddoak.com>
2019-05-10 14:39:05 +0100

    NonVR - first pass remote grab

--- Assets/PolyPep/Scripts/MouseInteraction.cs
+++ Assets/PolyPep/Scripts/MouseInteraction.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class MouseInteraction : MonoBehaviour
 {
 
-    public Transform lastHit = null;
+	public Transform lastHit = null;
 	public Vector3 hitPoint;
 
 	public GameObject myMouseProxy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	public bool grabbing;
+	public bool startGrab;
+	public GameObject grabObj;
+	public float grabObjDist;
+	
+
+	// Start is called before the first frame update
+	void Start()
+	{
 		Camera.main.stereoTargetEye = StereoTargetEyeMask.None;
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateMouseRaycast();
-        UpdateMouseButtons();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateMouseRaycast();
+		//UpdateMouseButtons();
+		UpdatePollKeyboard();
+	}
 
 
 	void OnDrawGizmos()
@@ -33,9 +40,9 @@ public class MouseInteraction : MonoBehaviour
 
 
 	void UpdateMouseRaycast()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 		//Debug.Log(Input.mousePosition);
 
@@ -52,92 +59,195 @@ public class MouseInteraction : MonoBehaviour
 		//Debug.DrawRay(ray.origin, ray.direction * 100, Color.cyan);
 
 		if (Physics.Raycast(ray, out hit, 200.0f))
-        {
-            if (hit.transform)
-            {
+		{
+			if (hit.transform)
+			{
 				hitPoint = hit.point;
 				myMouseProxy.transform.position = hitPoint;
 
 				// raycast is hitting something
 
 				GameObject go = hit.transform.gameObject;
-                //Debug.Log(hit.transform.gameObject.name);
+				//Debug.Log(hit.transform.gameObject.name);
 
-                if (hit.transform != lastHit)
-                {
+				if (hit.transform != lastHit)
+				{
 					// something is not what I hit (or didn't hit) last frame
 					if (lastHit != null)
-                    {
+					{
 						// was hitting something last frame - but have exited it
 						HoverExit();
-                    }
+					}
 
 					// have entered something new
-                    HoverEnter(go);
+					HoverEnter(go);
 
-                }
+				}
 
 				// store current hit as lasthit
-                lastHit = hit.transform;
+				lastHit = hit.transform;
 
 
-            }
-            else
-            {
- 
-            }
+			}
+			else
+			{
 
-        }
-        else
-        {
-            if (lastHit != null)
-            {
-                HoverExit();
-                lastHit = null;
-            }
-        }
+			}
 
-    }
+		}
+		else
+		{
+			if (lastHit != null)
+			{
+				HoverExit();
+				lastHit = null;
+			}
+		}
 
-    void HoverExit()
-    {
-        GameObject go = lastHit.gameObject;
-        //Debug.Log(" Hover Exiting " + go.name + " GameObject");
-        BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
-        if (bu != null)
-        {
-            bu.SetBackboneUnitControllerHover(false);
-        }
-    }
+		// Remote Grab
 
-    void HoverEnter(GameObject go)
-    {
-       // Debug.Log(" Hover Entering " + go.name + " GameObject");
-        BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
-        if (bu != null)
-        {
-            //Debug.Log("      --> script");
-            bu.SetBackboneUnitControllerHover(true);
-        }
-    }
+		if (Input.GetMouseButtonDown(2) && lastHit && !grabbing)
+		{
+			startGrab = true;
+			grabbing = true;
+			grabObj = lastHit.gameObject;
+			grabObjDist = Vector3.Magnitude(hitPoint - Camera.main.transform.position);
+		}
 
-    private void UpdateMouseButtons()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Debug.Log(" Mouse Down");
-            if (lastHit != null)
-            {
-                GameObject go = lastHit.gameObject;
-                BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
-                if (bu != null)
-                {
-                    bu.SetMyResidueSelect(!bu.myResidue.residueSelected);
+		if (Input.GetMouseButtonUp(2))
+		{
+			grabbing = false;
+			grabObj = null;
+		}
 
-                }
-            }
-        }
+		if (grabbing)
+		{
+			Vector3 remoteGrabTargetPosition = ray.GetPoint(grabObjDist);
+			RemoteGrabInteraction(grabObj.transform, remoteGrabTargetPosition);
+		}
 
-    }
+	}
+
+	void HoverExit()
+	{
+		GameObject go = lastHit.gameObject;
+		//Debug.Log(" Hover Exiting " + go.name + " GameObject");
+		BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+		if (bu != null)
+		{
+			bu.SetBackboneUnitControllerHover(false);
+		}
+	}
+
+	void HoverEnter(GameObject go)
+	{
+		// Debug.Log(" Hover Entering " + go.name + " GameObject");
+		BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+		if (bu != null)
+		{
+			//Debug.Log("      --> script");
+			bu.SetBackboneUnitControllerHover(true);
+		}
+	}
+
+	private void UpdateMouseButtons()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			//Debug.Log(" Mouse Down");
+			if (lastHit != null)
+			{
+				GameObject go = lastHit.gameObject;
+				BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+				if (bu != null)
+				{
+					bu.SetMyResidueSelect(!bu.myResidue.residueSelected);
+
+				}
+			}
+		}
+
+	}
+
+	private void UpdatePollKeyboard()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			//Debug.Log(" Mouse Down");
+			if (lastHit != null)
+			{
+				GameObject go = lastHit.gameObject;
+				BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+				if (bu != null)
+				{
+					bu.SetMyResidueSelect(true);
+
+				}
+
+			}
+
+		}
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			//Debug.Log(" Mouse Down");
+			if (lastHit != null)
+			{
+				GameObject go = lastHit.gameObject;
+				BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+				if (bu != null)
+				{
+					bu.SetMyResidueSelect(false);
+
+				}
+
+			}
+
+		}
+	}
+
+	public void RemoteGrabInteraction(Transform t, Vector3 destination)
+	{
+		//Debug.Log("do  RemoteGrabInteraction!");
+
+		GameObject go = t.gameObject;
+		//BackboneUnit bu = (go.GetComponent("BackboneUnit") as BackboneUnit);
+		//if (bu != null)
+		{
+			//Debug.Log("      --> script");
+			//bu.TractorBeam(destination, true, 3.0f);
+			TractorBeam(go, destination, true, 5.0f);
+		}
+
+
+	}
+
+	public void TractorBeam(GameObject go, Vector3 position, bool attract, float scale)
+	{
+
+		//Debug.Log("tractor beam me!");
+
+		float tractorBeamAttractionFactor = scale * 100.0f;
+		float tractorBeamMin = scale * 100.0f;
+		float tractorBeamDistanceRatio = 400f / scale; // larger = weaker
+
+
+		Vector3 tractorBeam = position - go.transform.position;
+		float tractorBeamMagnitude = Vector3.Magnitude(tractorBeam);
+		//tractorBeamMagnitude = Mathf.Min(1.0f, tractorBeamMagnitude);
+
+		if (!attract)
+		{
+			// repel
+			tractorBeam = go.transform.position - position;
+		}
+		float tractorBeamScale = Mathf.Max(tractorBeamMin, (tractorBeamAttractionFactor * tractorBeamMagnitude / tractorBeamDistanceRatio));
+
+
+		go.GetComponent<Rigidbody>().AddForce((tractorBeam * tractorBeamScale), ForceMode.Acceleration);
+		// add scaling for 'size' of target?
+
+
+	}
+
 
 }
