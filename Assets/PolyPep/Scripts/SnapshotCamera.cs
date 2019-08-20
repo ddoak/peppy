7d2026b01cb767654b2087e67e215e7d2396c9c5
ddoak <spam@ddoak.com>
2019-08-20 18:20:03 +0100

    Fix for snapshot camera PNG rendering

--- Assets/PolyPep/Scripts/SnapshotCamera.cs
+++ Assets/PolyPep/Scripts/SnapshotCamera.cs
@@ -41,11 +41,12 @@ public class SnapshotCamera : MonoBehaviour {
 		RenderTexture currentRT = RenderTexture.active;
 		RenderTexture.active = Cam.targetTexture;
 
-		Cam.Render();
+		//Cam.Render(); // not necessary
 
 		Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
 		Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
 		Image.Apply();
+
 		RenderTexture.active = currentRT;
 
 		var Bytes = Image.EncodeToPNG();
